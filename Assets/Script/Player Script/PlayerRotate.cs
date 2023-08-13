30a2b980abc2c5eaa46b30bd23ca3e8b0a12d7cf
alfonsus57 <alfonsus@student.telkomuniversity.ac.id>
2023-08-13 15:06:07 +0700

    Double Jump

---
+++ Assets/Script/Player Script/PlayerRotate.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+
+        transform.up = direction;
+    }
+}
--- Assets/Script/Player Script/Shoot.cs
+++ Assets/Script/Player Script/Shoot.cs
@@ -5,23 +5,49 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class Shoot : MonoBehaviour
 {
+    int jump = 0;
+    Rigidbody2D rb;
+
     public GameObject senjata;
     public GameObject peluru;
     public AudioClip impact;
     AudioSource audioSource;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        { Instantiate(peluru, senjata.transform.position, transform.rotation); 
-        audioSource.PlayOneShot(impact, 0.7F);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = 1;
         }
-        
+
+        if(jump == 1 && Input.GetMouseButtonDown(0))
+        {
+            rb.AddForce(transform.up * 1000);
+            jump = 0;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+
+        Instantiate(peluru, senjata.transform.position, senjata.transform.rotation); 
+
+        audioSource.PlayOneShot(impact, 0.7F);
+
+            
+        }
+
     }
+
+    
 }
